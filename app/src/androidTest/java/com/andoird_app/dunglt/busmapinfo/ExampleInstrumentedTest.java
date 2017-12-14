75dc8ae6bb2ce4fd4b196030e8769d715fb78d04
RyanLe <ryanleitvn@gmail.com>
2017-12-14 16:05:51 +0700

    Init Bus Map Info Project

---
+++ app/src/androidTest/java/com/andoird_app/dunglt/busmapinfo/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.andoird_app.dunglt.busmapinfo;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.andoird_app.dunglt.busmapinfo", appContext.getPackageName());
+    }
+}
