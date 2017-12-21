bb87b78b3b6d5f66fc154a77a7c36ca1a985775e
RyanLe <ryanleitvn@gmail.com>
2017-12-21 23:06:18 +0700

    Add source for get current location

--- app/src/main/java/com/andoird_app/dunglt/busmapinfo/BusMapsActivity.java
+++ app/src/main/java/com/andoird_app/dunglt/busmapinfo/BusMapsActivity.java
@@ -1,18 +1,40 @@
 package com.andoird_app.dunglt.busmapinfo;
 
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class BusMapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private static final String TAG = "MapActivity";
+
+    private static final String FIND_PERMISSION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_PERMISSION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private  static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+
+    //vars
+    private Boolean mLocationPermissionsGranted = false;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +46,33 @@ public class BusMapsActivity extends FragmentActivity implements OnMapReadyCallb
         mapFragment.getMapAsync(this);
     }
 
+    private void getDeviceLocation(){
+        Log.d(TAG, "getDeviceLocation: getting the devices current location");
 
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        try {
+            Task location = mFusedLocationProviderClient.getLastLocation();
+            location.addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    if (task.isSuccessful()) {
+                        Log.d(TAG, "onComplete: found location");
+                        Location currentLocation = (Location) task.getResult();
+                    } else {
+                        Log.d(TAG, "onComplete: current location is null");
+                        Toast.makeText(BusMapsActivity.this, "unable to get current location", Toast.LENGTH_SHORT);
+                    }
+                }
+            });
+        }catch (SecurityException e){
+            Log.d(TAG, "getDeviceLocation: Security Exception: "+ e.getMessage());
+        }
+    }
+
+    private void moveCamera(LatLng latlng, float zoom){
+        //Log.d
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -36,11 +84,48 @@ public class BusMapsActivity extends FragmentActivity implements OnMapReadyCallb
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        Toast.makeText(this, "Map is already!",Toast.LENGTH_SHORT);
+
+        getLocationPermission();
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        /*LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+    }
+
+    private void getLocationPermission(){
+        Log.d(TAG, "request permission.");
+
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),FIND_PERMISSION) == PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),COARSE_PERMISSION) == PackageManager.PERMISSION_GRANTED){
+                mLocationPermissionsGranted = true;
+            }else{
+                ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        }else{
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode)
+        {
+            case LOCATION_PERMISSION_REQUEST_CODE:{
+                if(grantResults.length >0){
+                    for (int i =0; i < grantResults.length; i ++){
+                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                                mLocationPermissionsGranted = false;
+                                return;
+                        }
+                    }
+                    mLocationPermissionsGranted = true;
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/andoird_app/dunglt/busmapinfo/MainActivity.java
@@ -0,0 +1,70 @@
+package com.andoird_app.dunglt.busmapinfo;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
+/**
+ * Created by WIN10 on 12/21/2017.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (isServicesOK()){
+            init();
+        }
+    }
+
+    private void init(){
+        Button btnMap = (Button) findViewById(R.id.btnMap);
+
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BusMapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public boolean isServicesOK()
+    {
+        //everything is find and user can make map request
+        Log.d(TAG, "isServicesOk: check google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if(available == ConnectionResult.SUCCESS){
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
+            //an error occured but we can reslove it
+            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+
+            dialog.show();
+        }else{
+            Toast.makeText(this, "You can't make map request", Toast.LENGTH_SHORT).show();
+        }
+
+        return false;
+    }
+
+}
