491284204e61f981de247ce125db21961b88ca12
RyanLe <ryanleitvn@gmail.com>
2017-12-30 23:31:11 +0700

    add bus station detail activity

--- app/src/main/java/com/andoird_app/dunglt/busmapinfo/BusStationDetailActivity.java
+++ app/src/main/java/com/andoird_app/dunglt/busmapinfo/BusStationDetailActivity.java
@@ -3,15 +3,17 @@ package com.andoird_app.dunglt.busmapinfo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Toast;
 
 /**
  * Created by WIN10 on 12/30/2017.
  */
 
-public class BusStationDetailActivity extends FragmentActivity {
-
+public class BusStationDetailActivity extends AppCompatActivity {
+    //TabHost tabHost;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,5 +22,24 @@ public class BusStationDetailActivity extends FragmentActivity {
         Intent i = getIntent();
         Integer position = i.getIntExtra ("position",1);
         Toast.makeText(BusStationDetailActivity.this, "Item "+ Integer.toString(position)+ " is clicked!", Toast.LENGTH_SHORT).show();
+
+        android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Bus Station Detail");
+        toolbar.setNavigationIcon(getDrawable(R.drawable.ic_navbar));
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(BusStationDetailActivity.this, "Icon navigation click", Toast.LENGTH_SHORT).show();
+            }
+        });
+        setSupportActionBar(toolbar);
+
+        final ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
+
     }
+
+
+
 }
