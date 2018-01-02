20d4e88f738325e3feacc3505b37c18fa41525a7
RyanLe <ryanleitvn@gmail.com>
2018-01-02 21:45:58 +0700

    add map for bus detail direction tab

--- app/src/main/java/com/andoird_app/dunglt/busmapinfo/DirectionTab.java
+++ app/src/main/java/com/andoird_app/dunglt/busmapinfo/DirectionTab.java
@@ -4,9 +4,18 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
 /**
@@ -17,7 +26,11 @@ import android.view.ViewGroup;
  * Use the {@link DirectionTab#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DirectionTab extends Fragment {
+public class DirectionTab extends Fragment implements OnMapReadyCallback {
+
+    final static String TAG = "DirectionTab";
+    private GoogleMap mMap;
+    Context mContext;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -63,8 +76,44 @@ public class DirectionTab extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_direction_tab, container, false);
+    }
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        final SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.bus_detail_map);
+        if(mapFragment != null){
+            Log.d("DirectionTab","mapFragment is not null");
+            Toast.makeText(super.getContext(), "mapFragment is not null", Toast.LENGTH_SHORT).show();
+            mapFragment.getMapAsync(this);
+        }else{
+            Log.d("DirectionTab","mapFragment is null");
+            Toast.makeText(super.getContext(), "mapFragment is null", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        Log.d(TAG, "onMapReady: googlemap is alreadt. ");
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     // TODO: Rename method, update argument and hook method into UI event
